ae8383e9b5cbb9e2fb25d0708e5ba0b9672cb6a5
minhajspace <minhaj.space@gmail.com>
2020-12-09 01:34:01 +0530

    api is working fine

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-const portNumber = process.env.PORT  || 4000 ; 
+const portNumber = process.env.PORT  || 5000 ; 
 
 // requiring datebase connection file 
 
@@ -10,9 +10,18 @@ require('./db/connection')
 
 const student = require('./models/students')
 // create a new studnets 
-
-
-app.post('/students', (req,res)=>{
- res.send("hello this is student page of this project")
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.send("hello this is home page ")
 })
-app.listen(portNumber)
+
+app.post('/students', (req, res) => {
+    console.log(req.body)
+    const user = new student(req.body)
+    res.send("hello this is home page ")
+})
+
+
+app.listen(portNumber,()=>{
+    console.log(`connection is on portnumber ${portNumber}`)
+})
